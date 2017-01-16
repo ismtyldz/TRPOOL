fba88a2fa03748b7240b088fc01e3ec94bcc9022
ismtyldz <27ismetyildiz@gmail.com>
2017-01-16 09:59:22 +0200

    uygulama içerisinde kullanıcının ıd ve tokenınn tutabilmek amacıyla bilgiler sayfasının oluşturulması

---
+++ app/src/main/java/com/example/ismet/trpool/bilgiler.java
@@ -0,0 +1,13 @@
+package com.example.ismet.trpool;
+
+/**
+ * Created by ismet on 11.01.2017.
+ */
+public class bilgiler {
+    static public String token="";
+    static public String id="";
+
+
+
+
+}
